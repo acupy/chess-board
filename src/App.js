0cb3d2e79d8083ebd7866cae8145ebc8ca4f75c4
acupy <adam.czuprak@hotmail.com>
2020-05-09 19:27:22 +0200

    closable config panel

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
     this.onStyleChanged = this.onStyleChanged.bind(this);
     this.onThemeChanged = this.onThemeChanged.bind(this);
     this.onChessRuleEnforcementChanged = this.onChessRuleEnforcementChanged.bind(this);
+    this.onToggleConfig = this.onToggleConfig.bind(this);
 
     // Forsyth–Edwards Notation
     const startPosition = 'r1bqkbnr/pppp1ppp/8/1B2p3/3nP3/5N2/PPPP1PPP/RNBQK2R';
@@ -20,7 +21,8 @@ class App extends Component {
       textInput: startPosition,
       position: startPosition,
       pieceStyle: 'alpha',
-      chessRulesEnforced: false,
+      chessRulesEnforced: true,
+      showConfig: false,
     };
   }
 
@@ -48,6 +50,10 @@ class App extends Component {
     this.setState({chessRulesEnforced: event.target.checked});
   }
 
+  onToggleConfig(event) {
+    this.setState({showConfig:!this.state.showConfig});
+  }
+
   render() {
     return <div>
       <header>
@@ -57,7 +63,8 @@ class App extends Component {
             <option value='gray'>gray</option>
             <option value='purple'>purple</option>
           </select>
-      </div>
+        </div>
+        <div className='config-button' onClick={this.onToggleConfig}>⚙️</div>
       </header>
       <div className='content-container'>
         <Board
@@ -65,25 +72,31 @@ class App extends Component {
           pieceStyle={this.state.pieceStyle}
           chessRulesEnforced={this.state.chessRulesEnforced}
           onBoardUpdated={this.updateFEN} />
+        {this.state.showConfig &&
         <div className='config-panel'>
-          <div className='header-small'>Config</div>
-          <label>FEN 
-            <input type='text' value={this.state.textInput} onChange={this.onFENChanged} />
-          </label>
-          <label>Board style
-            <select onChange={this.onStyleChanged}>
-              <option value='alpha'>alpha</option>
-              <option value='cheq'>cheq</option>
-              <option value='leipzig'>leipzig</option>
-            </select>
-          </label>
-          <label style={{flexFlow: 'row'}}>Enforce chess rules: 
-            <input 
-              type='checkbox' 
-              defaultChecked={this.state.chessRulesEnforced} 
-              onChange={this.onChessRuleEnforcementChanged}/>
-          </label>
-        </div>
+          <div className='config-panel-header'>
+            <div>Config</div>
+            <div className='close-btn' onClick={this.onToggleConfig}>✖️</div>
+          </div>
+          <div className='config-panel-body'>
+            <label>FEN 
+              <input type='text' value={this.state.textInput} onChange={this.onFENChanged} />
+            </label>
+            <label>Board style
+              <select onChange={this.onStyleChanged}>
+                <option value='alpha'>alpha</option>
+                <option value='cheq'>cheq</option>
+                <option value='leipzig'>leipzig</option>
+              </select>
+            </label>
+            <label style={{flexFlow: 'row'}}>Enforce chess rules: 
+              <input 
+                type='checkbox' 
+                defaultChecked={this.state.chessRulesEnforced} 
+                onChange={this.onChessRuleEnforcementChanged}/>
+            </label>
+          </div>
+        </div>}
       </div>
       <footer>2018</footer>
     </div>
